7b2327ee1ca6771006c5f5fbf4f5116db9f76a0b
liangwnele <502635097@qq.com>
2017-11-04 18:21:28 +0800

    dabaop

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,7 @@
     <LeftTarget></LeftTarget>
     <!-- 浮动弹框 -->
     <MyDialog :visible.sync="diaLogs.a">
-      <h1 slot="header">这里可能是一个页面标题</h1>
+      <h1 slot="header">页面标题</h1>
         <p >主要内容的一个段落。</p>
         <p>另一个主要段落。</p>
         <p slot="footer">这里有一些联系信息</p>
--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,16 +1,35 @@
 <template>
   <div id="topNav">
     <div id="topIcons">
-      <i @click="setDiaLogs(['a',true])" class="fa fa-user"></i>
-      <i class="fa fa-sign-out"></i>
-      <i class="fa fa-question-circle"></i>
+
+      <div v-for="(meun,index) in topMeuns" :key="index">
+        <i @click="setDiaLogs(['a',true])" :class="meun.icon"></i>
+      </div>
     </div>
     <div id="ctIcons" >
-      <i class="fa fa-line-chart"></i>
-      <i class="fa fa-wrench"></i>
-      <i class="el-icon-edit"></i>
-      <i class="fa fa-bell-o"></i>
-      <i class="fa fa-list-ul"></i>
+      <div v-for="(meun,index) in ctmeuns" :key="index"
+      @click="ckCT(index)"
+       class="ctIcons">
+        <el-badge :is-dot="meun.isdot" >
+          <i :class="meun.icon"></i>
+        </el-badge>
+      </div>
+    </div>
+    
+    <div id="meuns" v-show="meunShow">
+      <div v-for="(meunL1,index) in meuns" :key="index" class="meunL1">
+        <div class="meunL1Title" @click= "ckmeunL1(meunL1,index)">
+           <i :class="meunL1.icon"></i>
+        </div>
+        <div v-show="meunL1.level.length>0&&meunL1.show" class="meunL2">
+          <div v-for="(meunL2 ,j) in meunL1.level" 
+          :key="j" 
+          @click="ckmeunL2(meunL2,j,meunL1,index)"
+          class="l2Item">
+            {{meunL2.title}}
+          </div>
+        </div>
+      </div>
     </div>
     
   </div>
@@ -24,13 +43,95 @@ export default {
   components: {},
   computed: {},
   data() {
-    return {};
+    return {
+      meunShow: false,
+      topMeuns: [
+        {
+          icon: "fa fa-user"
+        },
+        {
+          icon: "fa fa-sign-out"
+        },
+        {
+          icon: "fa fa-question-circle"
+        }
+      ],
+      ctmeuns: [
+        {
+          icon: "fa fa-line-chart"
+        },
+        {
+          icon: "fa fa-wrench"
+        },
+        {
+          icon: "el-icon-edit"
+        },
+        {
+          icon: "fa fa-bell-o",
+          isdot: true
+        },
+        {
+          icon: "fa fa-list-ul"
+        }
+      ],
+      meuns: [
+        {
+          icon: "fa fa-home",
+          show: false,
+          level: []
+        },
+        {
+          icon: "fa fa-map-signs",
+          show: false,
+          level: [
+            {
+              title: "厂区A"
+            },
+            {
+              title: "厂区B"
+            }
+          ]
+        },
+        {
+          icon: "fa fa-eye",
+          show: false,
+
+          level: [
+            {
+              title: "热力情况"
+            },
+            {
+              title: "统计分析"
+            }
+          ]
+        },
+        {
+          icon: "fa fa-cog",
+          show: false,
+
+          level: []
+        }
+      ]
+    };
   },
   computed: {
     ...mapGetters(["diaLogs"])
   },
   methods: {
-    ...mapActions(["setDiaLogs"])
+    ...mapActions(["setDiaLogs"]),
+    ckCT(i) {
+      if(i==3){
+        this.setDiaLogs(['a',true])
+      }else if (i == 4) {
+        this.meunShow = !this.meunShow;
+      }
+    },
+    ckmeunL1(l1, i) {
+      l1.show = true;
+    },
+    ckmeunL2(l2, j, l1, i) {
+      l1.show = false;
+    }
   },
   mounted() {}
 };
@@ -43,25 +144,81 @@ export default {
   top: 57px;
   width: 100%;
   height: 40px;
+  color: #eee;
 }
 #topIcons {
   position: absolute;
   top: -40px;
   right: 15px;
+  height: 100%;
+}
+#topIcons div {
+  display: inline-block;
+  width: 40px;
+  height: 100%;
+  line-height: 40px;
+  text-align: center;
+  font-size: 23px;
 }
 #ctIcons {
   display: flex;
   justify-content: flex-end;
   padding-right: 15px;
+  height: 100%;
 }
-i {
-  line-height: 40px;
-  color: #ffffff;
-  padding: 0 7px;
-  font-size: 23px;
+.ctIcons {
+  width: 40px;
+  height: 100%;
+  text-align: center;
+  font-size: 22px;
 }
-i:hover{
-  cursor:pointer;
+
+#topIcons div:hover,
+.ctIcons:hover {
+  cursor: pointer;
   color: #7f88e6;
+  background: rgba(24, 27, 58, 0.71);
+}
+
+#meuns {
+  position: absolute;
+  right: 15px;
+}
+.meunL1 {
+  margin-top: 10px;
+  position: relative;
+}
+.meunL1Title {
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  background: #20234c;
+  border: 1px solid #fff;
+  line-height: 40px;
+  text-align: center;
+}
+.meunL2 {
+  position: absolute;
+  top: 0px;
+  right: 50px;
+  border: 0.5px solid #fff;
+}
+.l2Item {
+  padding: 5px 10px;
+  min-width: 100px;
+  background: #181b3a;
+}
+.l2Item:hover {
+  background: #20234c;
+}
+.meunL2::before {
+  content: "";
+  position: absolute;
+  right: -10px;
+  top: 15px;
+  border-color: transparent;
+  border-style: solid;
+  border-left-color: #fff;
+  border-width: 5px;
 }
 </style>
